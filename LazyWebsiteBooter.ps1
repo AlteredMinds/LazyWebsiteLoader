91001caf28cdc12d05a78b56e0902ca90d43307c
AlteredMinds <116692103+AlteredMinds@users.noreply.github.com>
2024-09-04 21:07:55 -0500

    changed to use json file for configuration

--- LazyWebsiteBooter.ps1
+++ LazyWebsiteBooter.ps1
@@ -1,124 +1,114 @@
-﻿##############################################################################################################
-##################################### S T A R T   O F   C O N F I G ##########################################
-
-
-# Website variables you use for classes including the website urls. Remove or create variables to fit your needs.
-
-$otc = "https://my.otc.edu/"
-$netlab = "https://netlab.otc.edu/"
-$netacad = "https://www.netacad.com/"
-$testout = "https://labsimapp.testout.com/v6_0_583/index.html"
-$gitHub = "https://github.com/AlteredMinds"
-
-
-$classes = @(
-#########################################################################################################################
-# List of your classes. Each object represents a class and includes the following properties:                           #
-#  Name - The name of the class.                                                                                        #
-#  URLs - The websites you want to load for the class.                                                                  #
-#  Days - The days that you have class.                                                                                 #
-#  Times - The start and end times of class. Represented as a decimal in 24hr format (hour.min). Ex. (11.00, 13.05) is  #
-#          equal to 11:00am - 1:05pm.                                                                                   #
-#                                                                                                                       #
-#  Create or remove objects to match your number of classes. Then, customize the properties for each class.             #
-#  NOTE: The last object cannot have a ',' at the end, objects preceding the last object must.                          #
-#########################################################################################################################
-
-    ###   CLASS 1    ###
-    [PSCustomObject]@{
-        Name = "THE LINUX OPERATING SYSTEM"
-        URLs = @($otc, $testout, $netlab)
-        Days = @("Monday", "Wednesday")
-        Times = @(8.00, 10.30)
-    },
-
-    ###   CLASS 2   ###
-    [PSCustomObject]@{
-        Name = "FIREWALL ESSENTIALS"
-        URLs = @($otc, $netlab)
-        Days = @("Monday", "Wednesday")
-        Times = @(13.00, 15.05)
-    },
-
-    ###   CLASS 3  ###
-    [PSCustomObject]@{
-        Name = "CCNAv7: SRWE"
-        URLs = @($otc, $netacad, $netlab)
-        Days = @("Monday", "Wednesday")
-        Times = @(15.30, 23.03)
-    }
-
-####################################### E N D   O F   C O N F I G ############################################
-##############################################################################################################
-)
-
-#variables for the current date and time
-$dayOfWeek = (Get-Date).DayOfWeek
-$time = [float]((Get-Date).Hour + (Get-Date).Minute * 0.01)
-
-
-function CheckClass($_days, $_times)
+﻿# LOAD CONFIG
+$configPath = Join-Path -Path $PSScriptRoot -ChildPath "webloader_config.json"
+$configExists = Test-Path $configPath
+if (!$configExists)
 {
-    # Get the current day of the week
-    $currentDay = (Get-Date).DayOfWeek.ToString()
-
-    # Debug output to check the current day and the days array
-    Write-Host "Current Day: $currentDay"
-    Write-Host "Days Array: $($_days -join ', ')"
-
-    # Ensure $_days is an array and handle it correctly
-    if ($_days -is [Array] -and $_days -contains $currentDay)
-    {
-        # Output the current time and the time range being checked
-        Write-Host "Current Time: $time"
-        Write-Host "Class Time Range: $($_times[0]) to $($_times[1])"
-
-        # Check if the current time is within the class time range
-        if ($time -ge $_times[0] -and $time -lt $_times[1])
-        {
-            Write-Host "Class is currently ongoing."
-            return $true     
-        }
-        else
-        {
-            Write-Host "Class time does not match."
-        }
-    }
-    else
-    {
-        Write-Host "Current day is not a class day."
-    }
-    
-    return $false
+    Read-Host 'Missing Configuration file. Is "webloader_config.json" in the script directory?'
+    return
 }
 
-
-#logic beepboop oO
-foreach ($class In $classes)
+try
 {
-    $isCurrentClass = CheckClass($class.Days, $class.Times)
-    if ($isCurrentClass)
-    {
-            Write-Host "Current class:" $class.Name
-            foreach ($website In $class.URLs)
-            {
-                Write-Host "Starting" $website
-                Start-Process $website
-                Start-Sleep -Milliseconds 100
-            }        
-
+    $configData = Get-Content -Raw -Path $configPath | ConvertFrom-Json
+    $urls = $configData.urls
+    $classes = foreach ($class in $configData.classes) {
+        [PSCustomObject]@{
+            Name  = $class.Name
+            Room  = $class.Room
+            URLs  = $class.URLs | ForEach-Object { $urls.$_ }
+            Days  = $class.Days
+            Times = $class.Times
+        }
     }
 }
+catch
+{
+    Write-Host 'Failed to load configuration' -ForegroundColor Yellow
+    Write-Host 'Configuration file is improperly formatted. Ensure that "webloader_config.json" is in the proper JSON format.'
+    Write-Host ''
+    Read-Host 'Press Enter to abort'
+    return
+}
 
-#Creates a scheduled task to run this script at login if it doesnt exist
+#TASK SCHEDULE
 $taskName = "Start Websites at Login"
 $taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -like $taskName }
-
 if(!$taskExists) 
 {
+    if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) 
+    {
+        if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) 
+        {
+            $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+            Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+            Exit
+        }
+    }
     $args = '-command ' + $MyInvocation.MyCommand.Path
     $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument $args
     $trigger = New-ScheduledTaskTrigger -AtLogOn
-    Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger *> $null
-    Write-Host 'Created new scheduled task! Your websites will now load at login.'
+    try
+    {
+        Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger *> $null
+        Read-Host 'A new scheduled task was created! Your websites will now load at login. Dont forget to modify the script to match your schedule and needs. Press ENTER to continue'
+    }
+    catch
+    {
+        Read-Host 'Failed to create a new scheduled task. Press ENTER to continue'
+    }
 }
+
+function Verify-Class
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string[]]$_days,
+        
+        [Parameter(Mandatory=$true)]
+        [float[]]$_times
+    )
+
+    $timeMod = 0 #Debug variable
+    $currentDay = (Get-Date).DayOfWeek #'Monday'
+    $currentTime = [float](((Get-Date).Hour + $timeMod) + (Get-Date).Minute * 0.01)
+    if ($_days -contains $currentDay)
+    {
+        if ($currentTime -ge $_times[0] -and $currentTime -lt $_times[1])
+        {
+            return $true     
+        }
+    }  
+    return $false
+}
+
+Write-Host 'Initiating boot sequence......'
+Start-Sleep -Milliseconds 200
+foreach ($class In $classes)
+{
+    Write-Host "> Scanning space and time for" $class.Name
+    Start-Sleep -Milliseconds 100
+    $isCurrentClass = Verify-Class -_days $class.Days -_times $class.Times
+    if ($isCurrentClass)
+    {
+        Write-Host $class.Name 'found in Sector' $class.Room
+        Write-Host 'Starting loading sequence.....'
+        Start-Sleep -Milliseconds 100
+        foreach ($website In $class.URLs)
+        {
+            $newrl = $website -replace '^https?://([^/]+).*', '$1'
+            $ip = [System.Net.Dns]::GetHostAddresses($newrl)[0].IPAddressToString;
+            $refNum = ""
+            for ($i = 0; $i -lt 14; $i++)
+            {
+                $refNum = $refNum + [int](Get-Random -Minimum 0 -Maximum 9)
+            }
+            Write-Host "Opening portal to" $newrl
+            Write-Host " Link established via" $ip "Ref#$refNum"
+            Start-Process $website
+            Start-Sleep -Milliseconds 100
+        }
+        Write-Host "Loading sequence Complete." 
+        return
+    }    
+}
+Write-Host "Aborting... No classes found. Try again later or modify the script configuration.."
